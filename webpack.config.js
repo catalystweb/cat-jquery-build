fe11b49bc4416abfe05605b65a37b8126918b9d8
Daniel Kandilas <daniel@catalystweb.com.au>
2019-02-28 23:19:18 +1100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,13 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    'bundle.min.css': [
-      path.resolve(__dirname, 'src/css/base.css'),
-      path.resolve(__dirname, 'src/css/arrow-icon.css'),
-      path.resolve(__dirname, 'src/css/search-module.css'),
-      path.resolve(__dirname, 'src/css/user-module.css'),
-      path.resolve(__dirname, 'src/css/user-state.css')
-    ],
-    'bundle.js': [
-      path.resolve(__dirname, 'src/js/main.js')
-    ]
-  }, 
+  entry: './src/js/main.js',
   output: {
-    filename: '[name]',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      },
-    ],
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     contentBase: './dist'
   },
-  plugins: [
-    new ExtractTextPlugin("bundle.min.css"),
-  ]
 };

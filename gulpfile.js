6648ea38ac758cfde1d00ab5ba3ef1d672650aab
Daniel Kandilas <daniel@catalystweb.com.au>
2019-06-26 05:20:00 +1000

    remove dependancies and update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var { src, dest } = require('gulp');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require("gulp-uglify");
+var minify = require("gulp-clean-css");
 var browserSync = require('browser-sync').create();
 
 gulp.task('scripts', function() {
     return src('src/js/*.js')
         .pipe(concat('app.min.js'))
+        .pipe(uglify())
         .pipe(dest('app/js'))
 }); 
 
@@ -18,9 +21,11 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// gulp.task('watch', ['browser-sync'], function () {
-//     gulp.watch("src/js/*.js", ['scripts']);
-//     gulp.watch("*.html").on('change', browserSync.reload);
-// });
+ gulp.task('watch', function(){
+    gulp.watch("src/js/*.js", gulp.series(concat));
+    gulp.watch("src/js/*.js").on('change', browserSync.reload);
+    gulp.watch("src/css/*.css").on('change', browserSync.reload);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
 
-gulp.task('run', gulp.parallel('scripts', 'browser-sync'));
+gulp.task('run', gulp.parallel('scripts', 'watch', 'browser-sync'));

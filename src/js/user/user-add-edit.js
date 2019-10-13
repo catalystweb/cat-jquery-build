bf818f4e6f4dda0fa9d3ab72f6cad291491bba8e
Daniel Kandilas <daniel@catalystweb.com.au>
2019-10-13 16:15:16 +1100

    fix add user submit on no avatar

--- src/js/user/user-add-edit.js
+++ src/js/user/user-add-edit.js
@@ -123,9 +123,11 @@ $(document).on("click", function (e) {
     //add user submit button 
     if (e.target.id == "add-button") {
       if ($("#add-avatar option").filter(":selected").text() != "Use example Avatar") {
-        $("#add-avatar-ul").val('');
+        $("#add-avatar-ul").val('');        
         userAdd();
-      }    
+      } else {
+        userAdd();
+      }   
     }
 
     //hide show custom avatar button 
